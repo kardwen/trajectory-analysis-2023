b10caee2823632f3c35192c8022464e1afd1b414
Karl Felix Schewe <karl.felix.schewe@uni-muenster.de>
2023-07-02 20:04:17 +0200

    Set axis aspect ratio to 1

--- main_template.py
+++ main_template.py
@@ -18,6 +18,10 @@ listOfTrajectories = utils.importTrajectories(str(trajectories_dir))
 # Visualize trajectories
 colorMap = matplotlib.colormaps["viridis"]
 colors = [*map(colorMap, np.linspace(0, 1, len(listOfTrajectories)))]
+
+plt.figure()
+plt.gca().set_aspect('equal', adjustable='box')
+
 for i, trajectory in enumerate(listOfTrajectories):
     x = [point.x for point in trajectory.points]
     y = [point.y for point in trajectory.points]
@@ -25,37 +29,39 @@ for i, trajectory in enumerate(listOfTrajectories):
         x,
         y,
         color=colors[i],
-        label=f"Trajectory {str(trajectory.number)}",
+        label=f"Trajectory {trajectory.number}",
     )
 
-plt.draw()
 plt.title("2D Trajectories")
 plt.xlabel("x")
 plt.ylabel("y")
 plt.grid(True)
-plt.pause(0.001)
 plt.show(block=True)
 
 
 # Simplify at least one of the trajectories with Douglas Peucker
 # and/or Sliding Window Algorithm
 trajectory = listOfTrajectories[4]
-print(f"Original trajectory # points: {str(len(trajectory.points))}")
+print(f"Original trajectory # points: {len(trajectory.points)}")
 simplifiedTrajectory1 = functions.douglasPeucker(trajectory, 0.0002)
-print(f"Simplified trajectory (epsilon=0.0002) # points: {str(len(simplifiedTrajectory1.points))}")
+print(f"Simplified trajectory (epsilon=0.0002) # points: {len(simplifiedTrajectory1.points)}")
 simplifiedTrajectory2 = functions.douglasPeucker(trajectory, 1.0)
-print(f"Simplified trajectory (epsilon=1.0) # points: {str(len(simplifiedTrajectory2.points))}")
+print(f"Simplified trajectory (epsilon=1.0) # points: {len(simplifiedTrajectory2.points)}")
 
 # Visualize original trajectory and its two simplifications
 colorMap = matplotlib.colormaps["viridis"]
 colors = [*map(colorMap, np.linspace(0, 1, 3))]
+
+plt.figure()
+plt.gca().set_aspect('equal', adjustable='box')
+
 x = [point.x for point in trajectory.points]
 y = [point.y for point in trajectory.points]
 plt.plot(
     x,
     y,
     color=colors[0],
-    label=f"Trajectory {str(trajectory.number)}",
+    label=f"Trajectory {trajectory.number}",
 )
 x = [point.x for point in simplifiedTrajectory1.points]
 y = [point.y for point in simplifiedTrajectory1.points]
@@ -63,7 +69,7 @@ plt.plot(
     x,
     y,
     color=colors[1],
-    label=f"Trajectory {str(simplifiedTrajectory1.number)} - epsilon=0.0002",
+    label=f"Trajectory {simplifiedTrajectory1.number} - epsilon=0.0002",
 )
 x = [point.x for point in simplifiedTrajectory2.points]
 y = [point.y for point in simplifiedTrajectory2.points]
@@ -74,13 +80,11 @@ plt.plot(
     label=f"Trajectory {str(simplifiedTrajectory2.number)} - epsilon=1.0",
 )
 
-plt.draw()
 plt.title("Simplified Trajectories")
 plt.xlabel("x")
 plt.ylabel("y")
 plt.grid(True)
 plt.legend()
-plt.pause(0.001)
 plt.show(block=True)
 
 

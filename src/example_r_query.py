6938898766f249281632b07a7a9c6a19a3d7ebbc
Karl Felix Schewe <karl.felix.schewe@uni-muenster.de>
2023-07-21 22:07:46 +0200

    Time region queries

--- src/example_r_query.py
+++ src/example_r_query.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+from codetiming import Timer
+
 from app import functions, utils
 from app.point import Point
 from app.r_tree import RTree
@@ -11,29 +13,36 @@ listOfTrajectories = utils.importTrajectories(str(trajectoriesDir))
 
 
 # Build R-tree with all given 62 trajectories
-points = []
-for trajectory in listOfTrajectories:
-    points.extend(trajectory.points)
+with Timer(text="Build R-tree in {:.4f} seconds"):
+    points = []
+    for trajectory in listOfTrajectories:
+        points.extend(trajectory.points)
 
-rTree = RTree(points)
+    rTree = RTree(points)
 
 # Query the trajectories using the built R-tree and the region.
 # Which trajectories lie in the given region?
 # This query should return the trajectories with ids 43, 45, 50, 71, 83
 queryRegion = Region(Point(0.0012601754558545508, 0.0027251228043638775, 0.0), 0.00003)
 
-foundTrajectories = functions.solveQueryWithRTree(
-    queryRegion, rTree, listOfTrajectories
-)
+with Timer(text="Queried points with R-tree in {:.4f} seconds."):
+    foundTrajectories = functions.solveQueryWithRTree(
+        queryRegion, rTree, listOfTrajectories
+    )
+
 if len(foundTrajectories) > 0:
     print(
         f"Found trajectories {[trajectory.number for trajectory in foundTrajectories]}",
-        "within region (search in R-Tree)",
+        "within region (search in R-tree)",
     )
 else:
     print("No trajectories match the query.")
 
-foundTrajectories = functions.solveQueryWithoutRTree(queryRegion, listOfTrajectories)
+with Timer(text="Queried points without R-tree in {:.4f} seconds."):
+    foundTrajectories = functions.solveQueryWithoutRTree(
+        queryRegion, listOfTrajectories
+    )
+
 if len(foundTrajectories) > 0:
     print(
         f"Found trajectories {[trajectory.number for trajectory in foundTrajectories]}",

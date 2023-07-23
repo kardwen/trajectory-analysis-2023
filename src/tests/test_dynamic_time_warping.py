83743e922c62870a3c4734990482a50583fad517
Karl Felix Schewe <karl.felix.schewe@uni-muenster.de>
2023-07-23 20:08:27 +0200

    Remove DTM tests without validated expected results

--- src/tests/test_dynamic_time_warping.py
+++ src/tests/test_dynamic_time_warping.py
@@ -38,80 +38,28 @@ def testEqualTrajectories():
             ],
         ),
     )
-    assert result == 0.0
+    assert result == approx(0.0)
 
 
 def testMinimalTrajectories():
     """
     Tests that it can handle trajectories only containing two points
+    The distance between all points are equal for easy calculation
     """
     result = dynamicTimeWarping(
         Trajectory(
             0,
             [
-                Point(0.1, 0.8, "2000-01-01 01:18:21"),
-                Point(0.2, 1.2, "2000-01-01 01:18:22"),
+                Point(1.0, 1.0, "2000-01-01 01:18:21"),
+                Point(2.0, 1.0, "2000-01-01 01:18:22"),
             ],
         ),
         Trajectory(
             1,
             [
-                Point(0.1, 1.2, "2000-01-01 01:18:21"),
-                Point(0.2, 1.4, "2000-01-01 01:18:22"),
+                Point(1.0, 2.0, "2000-01-01 01:18:21"),
+                Point(2.0, 2.0, "2000-01-01 01:18:22"),
             ],
         ),
     )
-    assert result == approx(0.2)
-
-
-def testTrajectories():
-    """
-    Tests two trajectories with different lengths and curvature
-    """
-    result = dynamicTimeWarping(
-        Trajectory(
-            0,
-            [
-                Point(0.1, 0.2, "2000-01-01 01:18:21"),
-                Point(0.5, 0.7, "2000-01-01 01:18:22"),
-                Point(0.8, 0.5, "2000-01-01 01:18:23"),
-            ],
-        ),
-        Trajectory(
-            1,
-            [
-                Point(0.1, -0.2, "2000-01-01 01:18:21"),
-                Point(0.3, 0.1, "2000-01-01 01:18:22"),
-                Point(0.5, 0.4, "2000-01-01 01:18:23"),
-                Point(0.8, -0.2, "2000-01-01 01:18:24"),
-            ],
-        ),
-    )
-    assert result == approx(0.3166667, rel=1e-4)
-
-
-def testParallelTrajectories():
-    """
-    Tests DTW distance of parallel trajectories
-    """
-    result = dynamicTimeWarping(
-        Trajectory(
-            0,
-            [
-                Point(0.1, 1.2, "2000-01-01 01:18:21"),
-                Point(0.5, 1.2, "2000-01-01 01:18:22"),
-                Point(0.8, 1.2, "2000-01-01 01:18:23"),
-                Point(1.1, 1.2, "2000-01-01 01:18:24"),
-            ],
-        ),
-        Trajectory(
-            1,
-            [
-                Point(0.1, 2.2, "2000-01-01 01:18:21"),
-                Point(0.5, 2.2, "2000-01-01 01:18:22"),
-                Point(0.8, 2.2, "2000-01-01 01:18:23"),
-                Point(1.1, 2.2, "2000-01-01 01:18:24"),
-            ],
-        ),
-    )
-    assert result == approx(1.0)
+    assert result == approx(2.0)
